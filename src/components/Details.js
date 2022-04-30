9f1d9ef1ea821fc182294d39379ba4bc6fed51ed
Stephani Rocha <gomes.stephani19@gmail.com>
2022-04-30 16:44:32 -0300

    Implementação requisito 11

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,6 +8,9 @@ class Details extends Component {
     super();
     this.state = {
       details: [],
+      email: '',
+      avaliacao: '',
+      avalia: 1, // aqui estamos em dúvida sobre como guardar o valor dos radios;
     };
   }
 
@@ -36,8 +39,25 @@ class Details extends Component {
     localStorage.setItem('cartItems', JSON.stringify(storageReturn));
   }
 
+  /*  addComent = () => {
+    const { } = this.props;
+    const itemsCart = listaDeProdutos.find((el) => el.id === target.name);
+    itemsCart.qtd = (itemsCart.qtd || 0) + 1;
+    const storageReturn = JSON.parse(localStorage.getItem('cartItems')) || [];
+    storageReturn.push(itemsCart);
+    localStorage.setItem('cartItems', JSON.stringify(storageReturn));
+  } */
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const { name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    const { details } = this.state;
+    const { details, email, avaliacao, avalia } = this.state;
     return (
       <div>
         <p>Detalhes</p>
@@ -65,6 +85,89 @@ class Details extends Component {
             Adicionar ao carrinho
           </button>
         </div>
+        <form>
+          <input
+            placeholder="Email"
+            type="email"
+            data-testid="product-detail-email"
+            value={ email }
+            name="email"
+            onChange={ this.handleChange }
+          />
+          <div>
+            <label htmlFor="comentario">
+              Deixe seu comentário
+              <textarea
+                id="comentario"
+                data-testid="product-detail-evaluation"
+                value={ avaliacao }
+                name="avaliacao"
+                onChange={ this.handleChange }
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="1">
+              1
+              <input
+                data-testid="1-rating"
+                id="1"
+                value={ avalia }
+                name="avalia"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="2">
+              2
+              <input
+                type="radio"
+                data-testid="2-rating"
+                id="2"
+                value={ avalia }
+                name="avalia"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="3">
+              3
+              <input
+                type="radio"
+                data-testid="3-rating"
+                id="3"
+                value={ avalia }
+                name="avalia"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="4">
+              4
+              <input
+                type="radio"
+                data-testid="4-rating"
+                id="4"
+                value={ avalia }
+                name="avalia"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="5">
+              5
+              <input
+                type="radio"
+                data-testid="5-rating"
+                id="5"
+                value={ avalia }
+                name="avalia"
+                onChange={ this.handleChange }
+              />
+            </label>
+          </div>
+          <div>
+            <button type="button" data-testid="submit-review-btn">
+              Avaliar
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
